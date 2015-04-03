61c81903b2a0475b513d1bf0e683cc9df58b2924
saneki <s@neki.me>
2015-04-03 13:32:29 -0500

    Support different salt ids

--- crypt.c
+++ crypt.c
@@ -5,20 +5,23 @@
 #include <getopt.h>
 
 #define CRYPT_ID_MD5 "1"
-#define CRYPT_ID_BLOWFISH "2a"
 #define CRYPT_ID_SHA256 "5"
 #define CRYPT_ID_SHA512 "6"
+
+// Leaving out blowfish, only available in some linux distributions
+// according to crypt(3) man page
+// #define CRYPT_ID_BLOWFISH "2a"
 
 typedef struct __crypt_args_t
 {
 	const char *pass;
 	const char *salt;
-	bool no_id;
+	const char *id;
 	bool no_newline;
 	bool help;
 } crypt_args_t;
 
-#define CRYPT_ARGS_DEFAULT { NULL, NULL, false, false, false }
+#define CRYPT_ARGS_DEFAULT { NULL, NULL, CRYPT_ID_SHA512, false, false }
 
 void parse_args(int argc, char *argv[], crypt_args_t *cargs);
 void print_help();
@@ -42,25 +45,33 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
-	if(cargs.salt == NULL)
+	if(cargs.salt == NULL || strlen(cargs.salt) == 0)
 	{
-		fprintf(stderr, "a salt is currently required, aborting\n");
+		fprintf(stderr, "a salt is required, aborting\n");
 		return 1;
 	}
 
-	// Force SHA512 for now
-	const char *id = CRYPT_ID_SHA512;
+	char *hashed;
 
-	int salt_len = strlen(cargs.salt);
-	int salt_id_len = strlen(id);
+	if(cargs.id == NULL)
+	{
+		hashed = crypt(cargs.pass, cargs.salt);
+	}
+	else
+	{
+		const char *id = cargs.id;
 
-	// Todo: Positive checks
+		int salt_len = strlen(cargs.salt);
+		int salt_id_len = strlen(id);
 
-	size_t full_len = (size_t)salt_len + (size_t)salt_id_len + 3;
-	char full_salt[full_len + 1];
-	sprintf(full_salt, "$%s$%s$", id, cargs.salt);
+		// Todo: Positive checks
 
-	char *hashed = crypt(cargs.pass, full_salt);
+		size_t full_len = (size_t)salt_len + (size_t)salt_id_len + 3;
+		char full_salt[full_len + 1];
+		sprintf(full_salt, "$%s$%s$", id, cargs.salt);
+
+		hashed = crypt(cargs.pass, full_salt);
+	}
 
 	// Print hashed password
 	if(cargs.no_newline) printf("%s", hashed);
@@ -71,16 +82,19 @@ int main(int argc, char *argv[])
 
 void parse_args(int argc, char *argv[], crypt_args_t *cargs)
 {
-	const char *argstr = "Ins:h?";
+	const char *argstr = "156Ins:h?";
 	extern char *optarg;
 	int option_index = 0;
 	int c;
 
 	struct option long_options[] =
 	{
+		{ "md5", no_argument,         0, '1' },
 		{ "no-id", no_argument,       0, 'I' },
 		{ "no-newline", no_argument,  0, 'n' },
 		{ "salt",  required_argument, 0, 's' },
+		{ "sha256", no_argument,      0, '5' },
+		{ "sha512", no_argument,      0, '6' },
 		{ "help",  no_argument,       0, 'h' },
 		{ 0, 0, 0, 0 }
 	};
@@ -89,8 +103,20 @@ void parse_args(int argc, char *argv[], crypt_args_t *cargs)
 	{
 		switch(c)
 		{
+			case '1':
+				cargs->id = CRYPT_ID_MD5;
+				break;
+
+			case '5':
+				cargs->id = CRYPT_ID_SHA256;
+				break;
+
+			case '6':
+				cargs->id = CRYPT_ID_SHA512;
+				break;
+
 			case 'I':
-				cargs->no_id = true;
+				cargs->id = NULL;
 				break;
 
 			case 'n':
@@ -120,7 +146,10 @@ void print_help()
 	printf("Usage: crypt [options] [-s salt] password\n");
 	printf("\n");
 	printf("Options:\n");
-	printf("  -I, --no-id        don't use a salt id (not yet implemented)\n");
+	printf("  -1, --md5          use crypt(3) MD5 salt id\n");
+	printf("  -5, --sha256       use crypt(3) SHA256 salt id\n");
+	printf("  -6, --sha512       use crypt(3) SHA512 salt id (default)\n");
+	printf("  -I, --no-id        don't use a salt id\n");
 	printf("  -n, --no-newline   don't print a newline after the hash\n");
 	printf("  -s, --salt         specify salt to use\n");
 	printf("  -h, -?, --help     output this message\n");
