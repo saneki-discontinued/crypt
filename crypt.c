6898ed7399d09a1210dc271228339a4944eed809
saneki <s@neki.me>
2015-04-05 17:30:19 -0500

    Added version

--- crypt.c
+++ crypt.c
@@ -4,6 +4,8 @@
 #include <string.h>
 #include <getopt.h>
 
+#define CRYPT_VERSION "1.0.0"
+
 #define CRYPT_ID_MD5 "1"
 #define CRYPT_ID_SHA256 "5"
 #define CRYPT_ID_SHA512 "6"
@@ -18,13 +20,15 @@ typedef struct __crypt_args_t
 	const char *salt;
 	const char *id;
 	bool no_newline;
+	bool version;
 	bool help;
 } crypt_args_t;
 
-#define CRYPT_ARGS_DEFAULT { NULL, NULL, CRYPT_ID_SHA512, false, false }
+#define CRYPT_ARGS_DEFAULT { NULL, NULL, CRYPT_ID_SHA512, false, false, false }
 
 void parse_args(int argc, char *argv[], crypt_args_t *cargs);
 void print_help();
+void print_version();
 
 int main(int argc, char *argv[])
 {
@@ -34,6 +38,11 @@ int main(int argc, char *argv[])
 	if(cargs.help)
 	{
 		print_help();
+		return 0;
+	}
+	else if(cargs.version)
+	{
+		print_version();
 		return 0;
 	}
 
@@ -81,7 +90,7 @@ int main(int argc, char *argv[])
 
 void parse_args(int argc, char *argv[], crypt_args_t *cargs)
 {
-	const char *argstr = "156Ins:h?";
+	const char *argstr = "156Ins:vh?";
 	extern char *optarg;
 	int option_index = 0;
 	int c;
@@ -94,6 +103,7 @@ void parse_args(int argc, char *argv[], crypt_args_t *cargs)
 		{ "salt",  required_argument, 0, 's' },
 		{ "sha256", no_argument,      0, '5' },
 		{ "sha512", no_argument,      0, '6' },
+		{ "version", no_argument,     0, 'v' },
 		{ "help",  no_argument,       0, 'h' },
 		{ 0, 0, 0, 0 }
 	};
@@ -126,6 +136,10 @@ void parse_args(int argc, char *argv[], crypt_args_t *cargs)
 				cargs->salt = optarg;
 				break;
 
+			case 'v':
+				cargs->version = true;
+				break;
+
 			case '?':
 			case 'h':
 				cargs->help = true;
@@ -142,6 +156,7 @@ void parse_args(int argc, char *argv[], crypt_args_t *cargs)
 
 void print_help()
 {
+	print_version();
 	printf("Usage: crypt [options] [-s salt] password\n");
 	printf("\n");
 	printf("Options:\n");
@@ -151,6 +166,12 @@ void print_help()
 	printf("  -I, --no-id        don't use a salt id\n");
 	printf("  -n, --no-newline   don't print a newline after the hash\n");
 	printf("  -s, --salt         specify salt to use\n");
+	printf("  -v, --version      output the version\n");
 	printf("  -h, -?, --help     output this message\n");
 	printf("\n");
+}
+
+void print_version()
+{
+	printf("crypt version %s\n", CRYPT_VERSION);
 }
